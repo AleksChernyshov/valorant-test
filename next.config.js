04fa868c6c6e45891dd09f5d1374e4fc12787f2f
AlexChernyshov <alex.chernyshov.dev@gmail.com>
2025-03-11 10:25:11 +0200

    update config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  basePath: isProd ? '/valorant-test' : '',
+  assetPrefix: isProd ? '/valorant-test/' : '',
   images: {
     domains: ["media.valorant-api.com"],
   },
